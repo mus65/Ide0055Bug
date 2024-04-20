73e6318e2b57c709e5ff6c2ac87902d918dc0ba2
Marius Thesing <marius.thesing@gmail.com>
2024-04-20 13:25:50 +0200

    IDE0055 bug

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Ide0055Bug
+{
+    /// <summary>
+    /// test.
+    /// </summary>
+#pragma warning disable TEST
+    [Serializable]
+#pragma warning restore TEST
+    public class Class1
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+To Reproduce, run
+
+`dotnet build`
+
+ or
+
+`dotnet format Ide0055Bug.csproj whitespace --verify-no-changes`
